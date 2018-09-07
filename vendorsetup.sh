e0d63eb50e9037f10878cbdcba03b5530bf6771f
DuiWeL28 <42545152+DuiWeL28@users.noreply.github.com>
2018-09-07 03:01:20 +0200

    Add files via upload

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while read -r device || [[ -n $device ]]
+do
+    add_lunch_combo "aoscp_$device-userdebug"
+    add_lunch_combo "lineageos_$device-n5110"
+    add_lunch_combo "LineageOS_$device-n5110"
+    add_lunch_combo "DuiWeL28_$device-n5110"
+    add_lunch_combo "samsung_$device-n5110"
+done < vendor/aoscp/aoscp.devices
